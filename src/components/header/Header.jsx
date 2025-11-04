1b459ac71c75571003bcee5bf36eb7c28194ad4f
tskorohod <tskorohod@lmteam.by>
2025-11-04 19:31:20 +0300

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import Nav from '../nav/Nav';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+	return (
+		<>
+			<div className={styles.header}>
+				
+				<div className={styles.logo}>
+						ArtGallery
+				</div>
+				<Nav></Nav>
+			</div>
+		</>
+	)
+}
+
+export default Header;
