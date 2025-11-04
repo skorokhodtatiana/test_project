993c16d5850989e60041d0a426b6310216016651
tskorohod <tskorohod@lmteam.by>
2025-11-04 19:32:00 +0300

    added new components

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,13 @@
+import styles from './Loading.module.scss';
+
+const Loading = () => {
+	return (
+		<>
+			<div className={styles.main}>
+				<h1>Loading...</h1>
+			</div>
+		</>
+	)
+}
+
+export default Loading;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import styles from './Main.module.scss';
+
+const Main = () => {
+	return (
+		<>
+			<div className={styles.main}>
+				
+				
+			</div>
+		</>
+	)
+}
+
+export default Main;
