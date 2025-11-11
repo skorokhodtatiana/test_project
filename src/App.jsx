09271ca6a1238d412188ca6c629e61f03f3fac5d
tskorohod <tskorohod@lmteam.by>
2025-11-11 22:17:06 +0300

    moved getting datas to other components

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,19 @@ import Header from "./components/header/Header";
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Catalog from './pages/catalog/Catalog';
+import { useDispatch } from 'react-redux';
+import { fetchProducts } from './store/itemsSlice';
+import { useEffect } from "react";
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const promise = dispatch(fetchProducts('https://api.artic.edu/api/v1/artworks?page=2&limit=100'));
+		return () => {
+			promise.abort();
+		}
+	}, [dispatch])
 
 	return (
 		<HashRouter>
--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -1,19 +1,19 @@
 import { Suspense } from 'react';
 import Loading from '../../components/loading/Loading';
 import { lazy } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchProducts } from '../../store/itemsSlice';
-import { useEffect } from "react";
+// import { useDispatch } from 'react-redux';
+// import { fetchProducts } from '../../store/itemsSlice';
+// import { useEffect } from "react";
 
 const Catalog = () => {
-	const dispatch = useDispatch();
+	// const dispatch = useDispatch();
 
-	useEffect(() => {
-		const promise = dispatch(fetchProducts('https://api.artic.edu/api/v1/artworks?page=2&limit=100'));
-		return () => {
-			promise.abort()
-		}
-	}, [dispatch])
+	// useEffect(() => {
+	// 	const promise = dispatch(fetchProducts('https://api.artic.edu/api/v1/artworks?page=2&limit=100'));
+	// 	return () => {
+	// 		promise.abort();
+	// 	}
+	// }, [dispatch])
 
 	const LazyCatalog = lazy(() => import('../../components/showcase/Showcase'));
 
