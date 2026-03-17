8e700cceb48d1640ec55f1396a7c4f05a4592158
tskorohod <tskorohod@lmteam.by>
2026-03-17 19:59:23 +0300

    added redux-resist to save items in cart when the browser is restarted

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
-import store from './store';
+import store, { persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/loading/Loading.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={<Loading/>} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</StrictMode>,
 )
